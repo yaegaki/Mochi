a17c4c9c289b3eaab9beb56c7958b16d952a4218
yaegaki <yaegaki.fino@gmail.com>
2019-05-12 23:29:27 +0900

    Add image content types.

--- Mochi/ContentTypes.cs
+++ Mochi/ContentTypes.cs
@@ -9,5 +9,10 @@ namespace Mochi
         public const string ApplicationXWWWFormURLEncoded = "application/x-www-form-urlencoded";
 
         public const string MultiplartFormData = "multipart/form-data";
+
+        public const string ImageXIcon = "image/x-icon";
+        public const string ImageJPEG = "image/jpeg";
+        public const string ImagePNG = "image/png";
+        public const string ImageGIF = "image/gif";
     }
 }
