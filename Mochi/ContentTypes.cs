a319e096b7927ea30bffc395937a46bbcbf64a4c
yaegaki <yaegaki.fino@gmail.com>
2019-05-17 03:32:53 +0900

    Add content type for js

--- Mochi/ContentTypes.cs
+++ Mochi/ContentTypes.cs
@@ -5,6 +5,7 @@ namespace Mochi
         public const string TextPlane = "text/plane";
         public const string TextHtml = "text/html";
 
+        public const string ApplicationJavaScript = "application/javascript";
         public const string ApplicationJSON = "application/json";
         public const string ApplicationXWWWFormURLEncoded = "application/x-www-form-urlencoded";
 
